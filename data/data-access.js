59e595470e817a756ffb4a043ea3ddaf41594fea
Paul Schlagbauer <paul.schlagbauer@edu.htl-villach.at>
2021-06-01 19:15:01 +0200

    Small style changes 2

--- data/data-access.js
+++ data/data-access.js
@@ -108,7 +108,7 @@ function createDao() {
 
     dao.getRate = (id, user) => {
         let hasRated = 0;
-        let ratings = fm.load(rating).data;
+        let ratings = fm.load(rating).data; 
         ratings.forEach(data => {
             if(data.owner == user && data.img == id) {
                 hasRated = data.type;
